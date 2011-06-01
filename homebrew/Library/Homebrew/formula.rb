3b6353a72574c7d48e30292d8267acbb5d7698ed
Aman Gupta <aman@tmm1.net>
2011-06-01 05:33:10 +0200

    read patches from cache dir

--- homebrew/Library/Homebrew/formula.rb
+++ homebrew/Library/Homebrew/formula.rb
@@ -568,7 +568,7 @@ EOF
           p[:curl_args] = [url, '-o', out_fn]
         else
           # it's a file on the local filesystem
-          p[:filename] = url
+          p[:filename] = HOMEBREW_CACHE+url
         end
 
         p[:args] = ["-#{arg}", '-i', p[:filename]]
