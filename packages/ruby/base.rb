07cfe08238f8d233da53b147aed94aa5a2b946ec
Aman Gupta <aman@tmm1.net>
2011-07-06 23:34:14 -0700

    install rubygems 1.6.2 and handle non-standard install prefix

--- packages/ruby/base.rb
+++ packages/ruby/base.rb
@@ -1,7 +1,7 @@
 class Ruby < DebianFormula
   section 'interpreters'
 
-  source 'http://production.cf.rubygems.org/rubygems/rubygems-1.8.5.tgz'
+  source 'http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz'
   source 'https://rubygems.org/downloads/bundler-1.0.15.gem'
 
   def post_install
@@ -9,19 +9,21 @@ class Ruby < DebianFormula
     (ENV.keys.grep(/^rvm/) + %w( RUBYOPT RUBYLIB GEM_HOME GEM_PATH MY_RUBY_HOME )).each do |env|
       ENV.delete(env)
     end
-    ENV['PATH'] = "#{destdir}/usr/bin:#{ENV['PATH']}"
+    ENV['PATH'] = "#{prefix/'bin'}:#{ENV['PATH']}"
 
     # paths to installed bins
-    ruby = destdir/'usr/bin/ruby'
-    gem = destdir/'usr/bin/gem'
+    ruby = prefix/'bin/ruby'
+    gem = prefix/'bin/gem'
 
     # setup RUBYLIB
     rubylib = `#{ruby} -e "puts $:.join(':')"`.strip
     raise unless $?.exitstatus == 0
-    ENV['RUBYLIB'] = rubylib.gsub('/usr', destdir/'usr')
+
+    pre = prefix.to_s.gsub(destdir, '')
+    ENV['RUBYLIB'] = rubylib.gsub(pre, prefix)
 
     # install rubygems
-    chdir(builddir/'rubygems-1.8.5') do
+    chdir(builddir/'rubygems-1.6.2') do
       sh ruby, 'setup.rb', '--no-ri', '--no-rdoc'
     end
 
@@ -30,7 +32,7 @@ class Ruby < DebianFormula
 
     # fix shebangs
     %w[ gem bundle ].each do |exe|
-      inreplace(destdir/'usr/bin'/exe, destdir, '')
+      inreplace(prefix/'bin'/exe, destdir, '')
     end
   end
 end
