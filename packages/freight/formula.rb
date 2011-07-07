37b78da5135b9f31845bae851653aace0dbaccde
Aman Gupta <aman@tmm1.net>
2011-07-06 20:30:26 -0700

    package for freight

---
+++ packages/freight/formula.rb
@@ -0,0 +1,28 @@
+class Freight < DebianFormula
+  homepage 'http://rcrowley.github.com/freight/'
+  url 'https://github.com/rcrowley/freight.git', :tag => 'v0.0.7'
+
+  arch 'all'
+  name 'freight'
+  section 'admin'
+  version '0.0.7'
+  description 'A modern take on the Debian archive'
+
+  conffiles '/etc/freight.conf'
+
+  def build
+  end
+
+  def install
+    make :install,
+      'DESTDIR' => destdir,
+      'prefix' => '/usr',
+      'sysconfdir' => '/etc'
+
+    mv etc/'freight.conf.example', etc/'freight.conf'
+
+    %w(lib cache).each do |dir|
+      (var/dir/'freight').mkpath
+    end
+  end
+end
