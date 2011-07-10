bf3118a0599910bf5465a1e9e797667e56b60c13
Aman Gupta <aman@tmm1.net>
2011-07-10 15:59:48 -0700

    libmemcache was a bust. use libmemcached instead

---
+++ packages/nginx/with_libmemcached_ssl.rb
@@ -0,0 +1,21 @@
+require './formula'
+
+class Nginx
+  version << "+libmemcached-ssl3"
+
+  build_depends \
+    'libmemcached-dev'
+
+  depends \
+    'libmemcached3'
+
+  def patches
+    {:p0 => 'request_start_variable.patch',
+     :p1 => 'https://github.com/tmm1/nginx/compare/master...libmemcached-ssl.patch'}
+  end
+
+  def configure(*args)
+    args.unshift '--with-memcache-ssl-session-store'
+    super(*args)
+  end
+end
