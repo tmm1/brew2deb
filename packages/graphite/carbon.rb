7f650e5841801c4dadaf2561229bb6016c6d3143
Aman Gupta <aman@tmm1.net>
2011-07-20 21:54:13 -0700

    convert gsub-ing in carbon package to use patches instead (thx @dcarley)

--- packages/graphite/carbon.rb
+++ packages/graphite/carbon.rb
@@ -4,7 +4,7 @@ class Carbon < DebianFormula
   md5 '611083ec9ad7418e7e72b962719204ae'
 
   name 'carbon'
-  version '0.9.8+github1'
+  version '0.9.8+github2'
   section 'python'
   description 'the Graphite backend'
 
@@ -16,32 +16,28 @@ class Carbon < DebianFormula
     'python-twisted',
     'python-pkg-resources'
 
-  requires_user 'graphite',
-    :home => '/var/lib/graphite',
+  requires_user 'carbon',
+    :home => '/var/lib/carbon',
     :chown => [
-      '/var/log/graphite',
-      '/var/lib/graphite',
-      '/var/run/graphite'
+      '/var/log/carbon',
+      '/var/lib/carbon'
     ]
 
   config_files \
-    '/etc/aggregation-rules.conf',
-    '/etc/carbon.conf',
-    '/etc/relay-rules.conf',
-    '/etc/rewrite-rules.conf',
-    '/etc/storage-schemas.conf'
+    '/etc/carbon/carbon.conf',
+    '/etc/carbon/storage-schemas.conf'
 
   def patches
-    'carbon-status-exit-code.patch'
+    [
+      'patches/carbon-setup.patch',
+      'patches/carbon-scripts.patch',
+      'patches/carbon-config.patch',
+      'patches/carbon-status-exit-code.patch',
+      'patches/carbon-udp-receiver.patch'
+    ]
   end
 
   def build
-    %w( lib/carbon/conf.py conf/carbon.conf.example ).each do |file|
-      inreplace file, "/opt/graphite/storage/whisper/", "/var/lib/graphite/whisper/"
-      inreplace file, "USER = ", "USER = graphite"
-    end
-
-    rm 'setup.cfg'
     sh 'python', 'setup.py', 'build'
   end
 
@@ -49,32 +45,16 @@ class Carbon < DebianFormula
     sh 'python', 'setup.py', 'install', "--prefix=#{prefix}"
 
     Dir[prefix/'bin/*.py'].each do |path|
-      inreplace path do |s|
-        # use system paths
-        s.gsub! /^STORAGE_DIR = .*$/, 'STORAGE_DIR = "/var/lib/graphite/"'
-        s.gsub! "LOG_DIR = join(STORAGE_DIR, 'log',", "LOG_DIR = join('/var', 'log', 'graphite',"
-        s.gsub! /^CONF_DIR = .*$/, 'CONF_DIR = "/etc/graphite/"'
-
-        # put pidfiles in /var/run
-        s.gsub! "'--pidfile', default=join(STORAGE_DIR,", "'--pidfile', default=join('/var/run/graphite',"
-
-        # we're installing into system lib paths
-        s.gsub! "sys.path.insert(0, LIB_DIR)\n", ''
-      end
-
-      # binaries don't need file extension
       mv path, path.gsub(/\.py$/,'')
     end
 
-    share.mkpath
-    mv prefix/'conf', share/'graphite'
+    (share/'carbon').install Dir['conf/*.example']
+    (etc/'carbon').install_p 'conf/carbon.conf.example', 'carbon.conf'
+    (etc/'init.d').install_p workdir/'init.d-carbon', 'carbon'
+    (var/'log/carbon').mkpath
+    (var/'run').mkpath
 
-    (etc/'graphite').mkpath
-    Dir[share/'graphite/*.example'].each do |conf|
-      cp conf, etc/'graphite'/File.basename(conf, '.example')
-    end
-
-    open etc/'graphite/storage-schemas.conf', 'w' do |f|
+    open etc/'carbon/storage-schemas.conf', 'w' do |f|
       f.puts %(
         [default]
         priority = 110
@@ -82,16 +62,5 @@ class Carbon < DebianFormula
         retentions = 10:2160,60:10080,600:262974
       ).ui
     end
-
-    %w( lib log run ).each do |dir|
-      (var/dir/'graphite').mkpath
-    end
-
-    rm_rf prefix/'storage/log'
-    mv Dir[prefix/'storage/*'], var/'lib/graphite'
-    rm_rf prefix/'storage'
-
-    (etc/'init.d').mkpath
-    cp workdir/'init.d-carbon', etc/'init.d/carbon'
   end
 end
