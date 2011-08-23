767107d6305c95ac6f359063dbab268468d1b184
Aman Gupta <aman@tmm1.net>
2011-08-22 18:09:42 -0700

    asciidoc backport

---
+++ packages/asciidoc/formula.rb
@@ -0,0 +1,10 @@
+class AsciiDoc < DebianSourceFormula
+  url 'http://backports.debian.org/debian-backports/pool/main/a/asciidoc/asciidoc_8.4.4-1~bpo50+1.dsc'
+  md5 '75db77d7ed1064dc8e28c1c8d0caaeeb'
+
+  version '8.4.4-1~bpo50+1+github1'
+
+  def patches
+    'eval-security-fix.patch'
+  end
+end
