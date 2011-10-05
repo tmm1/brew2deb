aa56802d509092cd8d1482ed3fe8b48f4cc449d7
Christian Paredes <cp@redbluemagenta.com>
2011-10-05 12:08:53 -0700

    Add credis-rpmtest for testing RPM packaging.

---
+++ packages/credis-rpmtest/formula.rb
@@ -0,0 +1,22 @@
+class Credis < RedHatFormula
+  homepage 'http://code.google.com/p/credis/'
+  url 'http://credis.googlecode.com/files/credis-0.2.3.tar.gz'
+  md5 '338c21667ece272d9ab669738e27b191'
+
+  name 'libcredis'
+  section 'devel'
+  version '0.2.3+github1'
+  description 'C client library for redis'
+
+  def build
+    make
+  end
+
+  def install
+    ['libcredis.a', 'libcredis.so'].each do |f|
+      lib.install f
+    end
+    include.install 'credis.h'
+    doc.install 'README'
+  end
+end
