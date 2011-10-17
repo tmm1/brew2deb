34259596824184cadc9d5c096000e7a903c8f64c
Aman Gupta <aman@tmm1.net>
2011-10-17 12:42:20 -0700

    basic android-sdk formula /cc @atmos

---
+++ packages/android-sdk/formula.rb
@@ -0,0 +1,31 @@
+class AndroidSDK < DebianFormula
+  homepage 'http://developer.android.com/sdk/index.html'
+  url 'http://dl.google.com/android/android-sdk_r13-linux_x86.tgz'
+  md5 'd80d7530a46c665644ae76084a9a0dc4'
+
+  name 'android-sdk'
+  version 'r13'
+  description 'Android SDK'
+  section 'devel'
+  arch 'all'
+
+  depends \
+    'ant',
+    'sun-java6-jdk'
+
+  def build
+  end
+
+  def install
+    (share/'android-sdk').install_p '.'
+  end
+
+  def post_inst
+    "
+      #!/bin/sh
+      set -e
+
+      /usr/share/android-sdk/tools/android update sdk --force --no-ui
+    ".ui
+  end
+end
