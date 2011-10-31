b388e762291b9a9be67b16ec3e5eb7bcd5426851
Corey Donohoe <atmos@atmos.org>
2011-10-31 16:36:34 -0700

    update the package name

--- packages/ruby/yarv.rb
+++ packages/ruby/yarv.rb
@@ -5,7 +5,7 @@ class YARV < Ruby
   homepage 'http://www.ruby-lang.org/'
   md5 '8e2fef56185cfbaf29d0c8329fc77c05'
 
-  name 'ruby-mri-1.9'
+  name 'ruby-yarv'
   section 'interpreters'
   version '1.9.3-p0'
   description 'The MRI Ruby virtual machine'
