879794f6eec65e6cefa143963282957ea68f0e84
Aman Gupta <aman@tmm1.net>
2011-12-14 16:44:59 -0800

    redis24 package

---
+++ packages/redis/redis24.rb
@@ -0,0 +1,9 @@
+require 'redis2'
+
+class Redis2
+  url 'http://redis.googlecode.com/files/redis-2.4.4.tar.gz'
+  md5 '8aec2c38b7d70956688cbfe3a99495fa'
+
+  name 'redis-server'
+  version '2.4.4+github1'
+end
