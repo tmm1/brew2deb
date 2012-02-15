9408f86d741915738cfc6e78d0bd6dc1a3264dab
Aman Gupta <aman@tmm1.net>
2012-02-15 15:48:02 -0800

    new lsof

--- packages/lsof/formula.rb
+++ packages/lsof/formula.rb
@@ -1,24 +1,24 @@
 class Lsof < DebianFormula
   homepage 'http://people.freebsd.org/~abe/'
-  url 'ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/lsof_4.84.tar.gz'
-  md5 '6dae655988c810a7042c06a4e2fa3c5f'
+  url 'ftp://lsof.itap.purdue.edu/pub/tools/unix/lsof/lsof_4.85.tar.gz'
+  md5 'e8151db708a023f588a3591da55a0a29'
 
   name 'lsof'
-  version '4.84'
+  version '4.85'
   section ''
   description 'lists information about files that are open by the processes running on a UNIX system'
 
   def build
-    sh 'tar xf lsof_4.84_src.tar'
-    chdir 'lsof_4.84_src' do
+    sh 'tar xf lsof_4.85_src.tar'
+    chdir 'lsof_4.85_src' do
       sh './Configure', '-n', 'linux'
       make
     end
   end
 
   def install
-    bin.install 'lsof_4.84_src/lsof'
-    man8.install 'lsof_4.84_src/lsof.8'
+    bin.install 'lsof_4.85_src/lsof'
+    man8.install 'lsof_4.85_src/lsof.8'
     sh 'gzip', man8/'lsof.8'
   end
 end
