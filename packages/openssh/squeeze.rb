012c9c94a7f9497fe4f739366ebaed03322c0b33
Aman Gupta <aman@tmm1.net>
2012-02-15 15:46:45 -0800

    squeeze version of mysql patched openssh

---
+++ packages/openssh/squeeze.rb
@@ -0,0 +1,14 @@
+require './formula'
+
+class OpenSSH
+  url 'http://ftp.de.debian.org/debian/pool/main/o/openssh/openssh_5.5p1-6+squeeze1.dsc'
+  md5 'b674c09fe06caa7b7735d05780813ae6'
+  version '1:5.5p1-6+squeeze1+github1'
+
+  def patches
+    [
+      'mysql_patch_5.5-p1-1.patch',
+      'squeeze-enable_mysql_keys.patch',
+    ]
+  end
+end
