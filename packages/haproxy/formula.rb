3ea1703f5fc6d622a597df22dea7d38222468841
Aman Gupta <aman@tmm1.net>
2012-03-01 17:39:17 -0800

    add init.d, example cfg, etc

--- packages/haproxy/formula.rb
+++ packages/haproxy/formula.rb
@@ -4,17 +4,23 @@ class Haproxy < DebianFormula
   md5 '41392d738460dbf99295fd928031c6a4'
 
   name 'haproxy'
-  version '1.4.19'
+  version '1.4.19+github1'
   section 'net'
   description 'The Reliable, High Performance TCP/HTTP Load Balancer'
 
   build_depends 'libpcre3-dev'
+  config_files '/etc/haproxy/haproxy.cfg'
 
   def build
     make 'TARGET' => 'linux26', 'CPU' => 'native', 'USE_PCRE' => '1', 'PREFIX' => '/usr'
   end
 
   def install
-    make :install, 'DESTDIR' => destdir, 'PREFIX' => '/usr', 'DOCDIR' => '/usr/share/doc'
+    make :install, 'DESTDIR' => destdir, 'PREFIX' => '/usr', 'DOCDIR' => '/usr/share/doc/haproxy'
+
+    (etc/'haproxy').install_p 'examples/haproxy.cfg'
+    (etc/'haproxy').install_p 'examples/errorfiles/', 'errors'
+    (etc/'default').install_p workdir/'default-haproxy', 'haproxy'
+    (etc/'init.d').install_p  workdir/'init.d-haproxy',  'haproxy'
   end
 end
