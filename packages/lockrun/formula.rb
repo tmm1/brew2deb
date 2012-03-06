e9b2197beb91543f0bc6babd1a6f8edb757efca3
Ben Burkert <ben@benburkert.com>
2012-03-06 14:56:45 -0800

    Update lockrun to version 3.

--- packages/lockrun/formula.rb
+++ packages/lockrun/formula.rb
@@ -1,10 +1,10 @@
 class Lockrun < DebianFormula
   homepage 'http://unixwiz.net/tools/lockrun.html'
-  url 'https://raw.github.com/gist/17e978b7af98d0a112c8/b23c8799c75ea504f5162e47be920a045c45242d/lockrun.c', :get => :curl
-  md5 '01b76b803fc8d0e9031cf06d0b06a809'
+  url 'https://raw.github.com/gist/17e978b7af98d0a112c8/eba757278acb425c0157d5d781f48a801905e7dc/lockrun.c', :get => :curl
+  md5 '7670ecfd1f9bf79462baed43e27af12f'
 
   name 'lockrun'
-  version '1.0+github2'
+  version '1.0+github3'
   description 'Run cron job with overrun protection'
 
   def build
