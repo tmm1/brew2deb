db8c426f1b7c98679c887ec1487818c0760ee1f7
Ben Burkert <ben@benburkert.com>
2012-04-12 18:15:19 -0700

    Replace bad mirror.

--- packages/openssl/formula.rb
+++ packages/openssl/formula.rb
@@ -1,4 +1,4 @@
 class OpenSSL < DebianSourceFormula
-  url 'http://backports.debian.org/debian-backports/pool/main/o/openssl/openssl_0.9.8o-4~bpo50+1.dsc'
+  url 'http://www.jp.backports.org/pool/main/o/openssl/openssl_0.9.8o-4~bpo50+1.dsc'
   md5 '0ce765777672340889a9cad03ee50fe6'
 end
