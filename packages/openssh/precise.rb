d07a5c7a529a2f311646234e9bb301697df79847
Aman Gupta <aman@tmm1.net>
2012-04-17 16:04:36 -0700

    precise version of patched openssh /cc @benburkert @sbryant

---
+++ packages/openssh/precise.rb
@@ -0,0 +1,19 @@
+require './formula'
+
+class OpenSSH
+  url 'http://archive.ubuntu.com/ubuntu/pool/main/o/openssh/openssh_5.9p1-5ubuntu1.dsc'
+  md5 'b67b3deb54e72a522153f5af33ef289e'
+  version '1:5.9p1-5ubuntu1+github1'
+
+  def patches
+    [
+      'mysql_patch_5.9-p1-5.patch',
+      'precise-enable_mysql_keys.patch',
+    ]
+  end
+
+  def patch
+    super
+    File.open('debian/source/options','w') {|f| f.puts 'auto-commit' }
+  end
+end
