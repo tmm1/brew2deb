867903143fec48a62fe5757ac634773166e70f1c
Ben Burkert <ben@benburkert.com>
2012-04-18 15:12:23 -0700

    Bump the github version of openssh.

--- packages/openssh/precise.rb
+++ packages/openssh/precise.rb
@@ -3,7 +3,7 @@ require './formula'
 class OpenSSH
   url 'http://archive.ubuntu.com/ubuntu/pool/main/o/openssh/openssh_5.9p1-5ubuntu1.dsc'
   md5 'b67b3deb54e72a522153f5af33ef289e'
-  version '1:5.9p1-5ubuntu1+github1'
+  version '1:5.9p1-5ubuntu1+github2'
 
   def patches
     [
