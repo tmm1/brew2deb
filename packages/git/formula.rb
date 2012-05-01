22d4d9781eeca86296990fec71417b61e48c3be5
Aman Gupta <aman@tmm1.net>
2012-05-01 16:36:03 -0700

    update git package to build from github/git instead of local patches

--- packages/git/formula.rb
+++ packages/git/formula.rb
@@ -1,11 +1,10 @@
 class Git < DebianFormula
-  url 'https://git-core.googlecode.com/files/git-1.7.10.tar.gz'
-  md5 'ab2716db51580037c7ebda4c8e9d56eb'
+  url 'https://github.com/github/git.git', :tag => 'github-1.7.10-2'
   homepage 'http://git-scm.com'
 
   section 'vcs'
   name 'git'
-  version '1:1.7.10-1+github1'
+  version '1:1.7.10-1+github2'
   description <<-DESC
     The Git DVCS with custom patches and bugfixes for GitHub.
   DESC
@@ -30,31 +29,6 @@ class Git < DebianFormula
   provides  'git-core', 'git-svn'
   replaces  'git-core', 'git-svn'
   conflicts 'git-core', 'git-svn'
-
-  def patches
-    [
-      # fix race condition and hide warnings in pack-objects
-      'patches/pack-object-race.patch',
-
-      # fire a hook on post-upload so we can track # of clones
-      'patches/post-upload-pack-hook.patch',
-
-      # fsckObjects doesn't show user warnings
-      'patches/receive-pack-fsck-object-warnings-non-fatal.patch',
-
-      # hide refs/pull/* via receive.hiderefs
-      'patches/receive-pack-hide-refs.patch',
-
-      # ignore invalid timezones
-      'patches/tz-fsck-warning.patch',
-
-      # github/git#1: statistics reporting
-      'patches/git-stats.patch',
-
-      # allow turning off .have lines entirely
-      'patches/receive-pack-advertise-alternates.patch',
-    ]
-  end
 
   def build
     make(vars)
