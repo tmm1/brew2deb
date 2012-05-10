9eb6d254e33a482a7f35bcb0bbe65693da550f5a
Jesse Newland <jesse@github.com>
2012-05-10 10:55:28 -0700

    ipvsadm

---
+++ packages/ipvsadm/formula.rb
@@ -0,0 +1,5 @@
+class Ipvsadm < DebianSourceFormula
+  url 'http://ftp.de.debian.org/debian/pool/main/i/ipvsadm/ipvsadm_1.26-1.dsc'
+  md5 'c589999623724ff1db4826c520cd79a0'
+  version '1:1.26-1+github1'
+end
