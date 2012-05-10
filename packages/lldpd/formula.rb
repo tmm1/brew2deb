ebfc4f0daae62290b3be0e0c8822c2a54ab099d1
Jesse Newland <jesse@github.com>
2012-05-10 10:52:15 -0700

    lldpd package

---
+++ packages/lldpd/formula.rb
@@ -0,0 +1,6 @@
+class Lldpd < DebianSourceFormula
+  url 'http://ftp.de.debian.org/debian/pool/main/l/lldpd/lldpd_0.5.1-1.dsc'
+  md5 'a42a97a679329210ac0729fdca2cdc1a'
+  version '0.5.1-1+github1'
+  build_depends 'libsnmp-dev'
+end
