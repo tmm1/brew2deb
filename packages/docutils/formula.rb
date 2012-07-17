8ef2127dfd63083d30990d369de3222ad760098d
tnm <ted@github.com>
2012-07-16 18:41:22 -0700

    Add docutils formula

---
+++ packages/docutils/formula.rb
@@ -0,0 +1,5 @@
+class Docutils < DebianSourceFormula
+  url 'https://launchpad.net/debian/experimental/+source/python-docutils/0.9.1-1/+files/python-docutils_0.9.1-1.dsc'
+  version '0.9.1-1'
+  md5 '2546cd3a93acc1ae63563b55d5465cc0'
+end
