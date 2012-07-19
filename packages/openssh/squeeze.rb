446644873ec0dd5bea7a770b7251c5fd51013742
Scott J. Goldman <scottjg@github.com>
2012-07-19 08:35:30 -0700

    openssh: bump package version numbers

--- packages/openssh/squeeze.rb
+++ packages/openssh/squeeze.rb
@@ -3,7 +3,7 @@ require './formula'
 class OpenSSH
   url 'http://ftp.de.debian.org/debian/pool/main/o/openssh/openssh_5.5p1-6+squeeze1.dsc'
   md5 'b674c09fe06caa7b7735d05780813ae6'
-  version '1:5.5p1-6+squeeze1+github4'
+  version '1:5.5p1-6+squeeze1+github5'
 
   def patches
     [
