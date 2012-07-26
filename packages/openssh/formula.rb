75d478b1c94c8f06552cba29de2a36cabbeaa8ec
Scott J. Goldman <scottjg@github.com>
2012-07-25 19:48:26 -0700

    openssh: bump version number to v8

--- packages/openssh/formula.rb
+++ packages/openssh/formula.rb
@@ -1,7 +1,7 @@
 class OpenSSH < DebianSourceFormula
   url 'http://ftp.de.debian.org/debian-archive/debian/pool/main/o/openssh/openssh_5.1p1-5.dsc'
   md5 '338282d6bc34e9ea227862557a042818'
-  version '1:5.1p1-5github7'
+  version '1:5.1p1-5github8'
 
   def patches
     [
