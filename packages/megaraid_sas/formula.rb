5a0cf5cc56599de01fc99ef3510971f9f243afa7
Scott J. Goldman <scottjg@github.com>
2012-07-26 21:39:14 -0700

    Change megaraid_sas to use dell driver

--- packages/megaraid_sas/formula.rb
+++ packages/megaraid_sas/formula.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Megaraid_sas < DebianFormula
-  url 'https://github.com/github/megaraid_sas.git', :sha => '4fb0dce821e0b1e179a9a2cf152676c442cd39f0'
+  url 'https://github.com/github/megaraid_sas.git', :sha => 'e6ba9b612eb7a6ddd5e76f7f8b940b95b2718d77'
 
   name 'megaraid-sas'
-  version '5.34-rc1+github4'
+  version '5.39+github1'
   section 'main'
   description 'GitHub\'s custom brewed MegaRAID SAS driver. Tastes like strawberries.'
 
@@ -13,12 +13,13 @@ class Megaraid_sas < DebianFormula
     'initramfs-tools'
 
   def build
-    system "make"
+    system "patch -p1 < patches/sles11-sp1.patch"
+    system "make PWD=`pwd` -f Makefile.standalone"
   end
 
   def install
     system "mkdir -p #{destdir}/lib/modules/2.6.32-5-amd64/updates/kernel/drivers/scsi/megaraid/"
-    system "cp src/*.ko #{destdir}/lib/modules/2.6.32-5-amd64/updates/kernel/drivers/scsi/megaraid/"
+    system "cp *.ko #{destdir}/lib/modules/2.6.32-5-amd64/updates/kernel/drivers/scsi/megaraid/"
   end
 
   def postinst
