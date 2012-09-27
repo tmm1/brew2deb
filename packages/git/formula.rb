d4f70c19192d66d462bfc2af78f75788dc25485c
Will Farrington <wfarr@github.com>
2012-09-27 10:02:06 -0700

    merge master

--- packages/git/formula.rb
+++ packages/git/formula.rb
@@ -1,10 +1,10 @@
 class Git < DebianFormula
-  url 'https://github.com/github/git.git', :tag => 'github-1.7.10-12'
+  url 'https://github.com/github/git.git', :tag => 'github-1.7.10-13'
   homepage 'http://git-scm.com'
 
   section 'vcs'
   name 'git'
-  version '1:1.7.10-1+github12'
+  version '1:1.7.10-1+github13'
   description <<-DESC
     The Git DVCS with custom patches and bugfixes for GitHub.
   DESC
