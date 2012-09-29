8b6ba85d7b40847227e5bfb082479f4891638cdf
Lee Huffman <huffman@github.com>
2012-09-28 15:30:17 -1000

    lshw formula

---
+++ packages/lshw/formula.rb
@@ -0,0 +1,10 @@
+class Lshw < DebianFormula
+  homepage 'http://ezix.org/project/wiki/HardwareLiSter'
+  url 'http://ezix.org/software/files/lshw-B.02.16.tar.gz'
+  md5 '67479167add605e8f001097c30e96d0d'
+
+  name 'lshw'
+  section 'utils'
+  version '0.2.16+github1'
+  description 'information about hardware configuration'
+end
