5e12cacd523086a0748a9d3f2517353a5d5c2484
Ted Nyman <ted@ted.io>
2012-10-17 00:35:50 -0700

    Bump zootest rev

--- packages/zootest/formula.rb
+++ packages/zootest/formula.rb
@@ -4,7 +4,7 @@ class Zootest < DebianFormula
   url 'https://github.com/tnm/zootest.git', :sha => '2f32f40383ad217d81eda0acf87239debc59d955'
 
   name 'zootest'
-  version '0.0.1-github'
+  version '0.0.2-github'
   description 'Universal packaging test'
 
   def build
