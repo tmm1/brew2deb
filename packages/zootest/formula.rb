fa1d71630ae8589577e4248a00e26f4fb0da9530
Ted Nyman <ted@ted.io>
2012-10-16 23:43:09 -0700

    Use the blank zootest for testing

---
+++ packages/zootest/formula.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Zootest < DebianFormula
+  url 'https://github.com/github/zootest.git', :sha => '1e850da21b33e03f0c005d7ced2eaddbbce7f3e2'
+
+  name 'zootest'
+  version '0.0.1-github'
+  description 'Universal packaging test'
+
+  def build
+  end
+
+  def install
+  end
+
+  def postinst
+  end
+end
