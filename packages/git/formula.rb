a495d41de722b6b89ef989451d6cbd4e65d0549e
Jeff King <peff@peff.net>
2012-10-18 05:44:11 -0400

    bump git to -github14
    Minor fix for a segfault on malformed query strings.

--- packages/git/formula.rb
+++ packages/git/formula.rb
@@ -1,10 +1,10 @@
 class Git < DebianFormula
-  url 'https://github.com/github/git.git', :tag => 'github-1.7.10-13'
+  url 'https://github.com/github/git.git', :tag => 'github-1.7.10-14'
   homepage 'http://git-scm.com'
 
   section 'vcs'
   name 'git'
-  version '1:1.7.10-1+github13'
+  version '1:1.7.10-1+github14'
   description <<-DESC
     The Git DVCS with custom patches and bugfixes for GitHub.
   DESC
