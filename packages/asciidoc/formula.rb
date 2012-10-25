d99d6f8c37137d38575bd74536fa593e1f51e8c0
Jesse Newland <jesse@github.com>
2012-10-25 14:52:33 -0700

    this is now in the archive

--- packages/asciidoc/formula.rb
+++ packages/asciidoc/formula.rb
@@ -1,5 +1,5 @@
 class AsciiDoc < DebianSourceFormula
-  url 'http://backports.debian.org/debian-backports/pool/main/a/asciidoc/asciidoc_8.4.4-1~bpo50+1.dsc'
+  url 'http://archive.debian.org/debian-backports/pool/main/a/asciidoc/asciidoc_8.4.4-1~bpo50+1.dsc'
   md5 '75db77d7ed1064dc8e28c1c8d0caaeeb'
 
   version '8.4.4-1~bpo50+1+github1'
