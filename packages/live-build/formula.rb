61ba3c6e0aed18dd0a2d8c2b0706eb102c6c5843
Tim Sharpe <tim@sharpe.id.au>
2012-12-14 09:49:37 +1100

    lenny version of debhelper works too

--- packages/live-build/formula.rb
+++ packages/live-build/formula.rb
@@ -1,4 +1,12 @@
 class LiveBuild < DebianSourceFormula
   url 'git://live.debian.net/git/live-build.git', :tag => 'debian/2.0.12-2'
   version '2.0.12-2+github1'
+
+  def build
+    inreplace "debian/control" do |s|
+      s.gsub!(/^Build-Depends: debhelper \(>= [\d\.]+~\)/, 'Build-Depends: debhelper (>= 7.0.15~)')
+    end
+
+    super
+  end
 end
