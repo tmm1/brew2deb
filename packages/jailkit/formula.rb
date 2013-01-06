eab7139533ca9d264d55baa65fb12760974512f9
Aman Gupta <aman@tmm1.net>
2013-01-06 03:57:13 -0800

    jailkit

---
+++ packages/jailkit/formula.rb
@@ -0,0 +1,10 @@
+class Jailkit < DebianFormula
+  homepage 'http://olivier.sessink.nl/jailkit/'
+  url 'http://olivier.sessink.nl/jailkit/jailkit-2.15.tar.bz2'
+  sha1 '85c78de913ec6d1edc6c0d1f2b6a2a4335b7a5a3'
+
+  name 'jailkit'
+  version '2.15'
+  description 'utilities for jailing a user or process'
+  section 'admin'
+end
