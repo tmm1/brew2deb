2250d8dd6b550492fac6f49ba59999bbf640b321
Scott J. Goldman <scottjg@github.com>
2013-01-13 19:46:51 -0800

    fix up ree-jemalloc formula to use jemalloc

--- packages/ruby/ree-jemalloc.rb
+++ packages/ruby/ree-jemalloc.rb
@@ -5,16 +5,16 @@ class REE < MRI
   url 'http://rubyenterpriseedition.googlecode.com/files/ruby-enterprise-1.8.7-2012.02.tar.gz'
   md5 '8d086d2fe68a4c57ba76228e97fb3116'
 
-  name 'rbenv-ree-1.8.7-2012.02+github1'
-  version '1.0.2+github1'
+  name 'rbenv-ree-jemalloc-1.8.7-2012.02'
+  version '1.0.2+jemalloc'
   description 'The REE Ruby virtual machine'
 
   build_depends \
     'ruby',
-    'google-perftools (>= 1.8.3+github2), google-perftools (<= 1.9)'
+    'jemalloc (>= 3.2.0)'
 
   depends \
-    'google-perftools (>= 1.8.3+github2), google-perftools (<= 1.9)'
+    'jemalloc (>= 3.2.0)'
 
   provides! 'rbenv-ree'
 
@@ -32,7 +32,7 @@ class REE < MRI
     ENV['CFLAGS'] = '-O2 -ggdb -Wall -fPIC -fno-builtin-malloc
      -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free
      -fno-stack-protector'.gsub(/\s+/, ' ')
-    ENV['LIBS'] = '-ltcmalloc'
+    ENV['LIBS'] = '-ljemalloc'
 
     sh './installer',
       '--no-dev-docs',
