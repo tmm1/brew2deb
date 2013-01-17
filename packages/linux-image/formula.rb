27b21ff9f92e73503b6b9c496a05c5d3159bd3b0
Scott J. Goldman <scottjg@github.com>
2013-01-17 14:16:50 -0800

    switch linux-image to use 3.2.0 from ubuntu 12.04

--- packages/linux-image/formula.rb
+++ packages/linux-image/formula.rb
@@ -1,23 +1,20 @@
 require 'formula'
 
 class LinuxImage < DebianFormula
-  url 'http://mirror.pnl.gov/ubuntu//pool/main/l/linux/linux-image-3.5.0-17-generic_3.5.0-17.28_amd64.deb'
-  md5 '957b98e7d921beb3d3f35cbdac827e3c'
-  source 'http://mirror.pnl.gov/ubuntu//pool/main/l/linux/linux-image-extra-3.5.0-17-generic_3.5.0-17.28_amd64.deb'
+  url 'http://mirror.pnl.gov/ubuntu//pool/main/l/linux/linux-image-3.2.0-35-generic_3.2.0-35.55_amd64.deb'
+  md5 'cd1a00401034c52310c1521d5656f9a3'
   source 'http://ftp.us.debian.org/debian/pool/main/l/linux-2.6/linux-image-2.6.32-5-amd64_2.6.32-46_amd64.deb'
 
   def build
-    sh 'ar x linux-image-3.5.0-*.deb'
+    sh 'ar x linux-image-3.2.0-*.deb'
     sh 'mkdir -p pkg'
-    sh 'cd pkg && tar jxf ../data.tar.bz2'
-    sh 'ar x linux-image-extra-3.5.0*.deb'
     sh 'cd pkg && tar jxf ../data.tar.bz2'
     sh 'ar x linux-image-2.6.32*.deb'
     sh 'mkdir -p pkg/DEBIAN'
     sh 'cd pkg/DEBIAN && tar zxf ../../control.tar.gz'
-    sh 'sed -i "s/2.6.32-5-amd64/3.5.0-17-generic/g" pkg/DEBIAN/*'
-    sh 'sed -i "s/Version: .*/Version: 3.5.0.17.28+github1/" pkg/DEBIAN/control'
-    sh 'sed -i "s/2.6.32 /3.5.0 /" pkg/DEBIAN/control'
+    sh 'sed -i "s/2.6.32-5-amd64/3.2.0-35-generic/g" pkg/DEBIAN/*'
+    sh 'sed -i "s/Version: .*/Version: 3.2.0-35.55+github1/" pkg/DEBIAN/control'
+    sh 'sed -i "s/2.6.32 /3.2.0 /" pkg/DEBIAN/control'
   end
 
   def install
@@ -25,6 +22,6 @@ class LinuxImage < DebianFormula
 
   def package
     sh 'dpkg -b pkg'
-    sh 'mv pkg.deb ../linux-image-3.5.0-17-generic_3.5.0-17.28+github1_amd64.deb'
+    sh 'mv pkg.deb ../linux-image-3.2.0-35-generic_3.2.0-35.55+github1_amd64.deb'
   end
 end
