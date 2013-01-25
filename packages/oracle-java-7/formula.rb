de5faf576cea8c72bbfcb912b412d37a392cad0f
James Fryman <james@frymanet.com>
2013-01-25 14:12:09 -0600

    initial commit of oracle java 7

---
+++ packages/oracle-java-7/formula.rb
@@ -0,0 +1,5 @@
+# dsc generated from https://github.com/rraptorr/oracle-java7
+class OracleJava7 < DebianSourceFormula
+  url 'https://s3.amazonaws.com/brew2deb/non-free/oracle-java7/oracle-java7_7.11-1.dsc'
+  md5 '86e843c6375acdb7e64fd93ef180f47b'
+end
