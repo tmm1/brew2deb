24814ff82f73bb12fd001c760b555c3cf640f6b6
Ben Burkert <ben@benburkert.com>
2013-01-31 12:59:18 -0800

    Bump s3fs to 1.62.

--- packages/s3fs/formula.rb
+++ packages/s3fs/formula.rb
@@ -1,10 +1,10 @@
 class S3fs < DebianFormula
   homepage 'http://code.google.com/p/s3fs/'
-  url 'http://s3fs.googlecode.com/files/s3fs-1.61.tar.gz'
+  url 'http://s3fs.googlecode.com/files/s3fs-1.62.tar.gz'
   sha1 '8f6561ce00b41c667b738595fdb7b42196c5eee6'
 
   name 's3fs'
-  version '1.61'
+  version '1.62'
   section ''
   description 'FUSE-based file system backed by Amazon S3'
 
