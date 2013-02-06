6956488b7e35f2d7a548adae908a7b37ef5284b1
Will Farrington <wfarr@github.com>
2013-02-06 14:35:08 -0800

    haveged formulad

---
+++ packages/haveged/formula.rb
@@ -0,0 +1,4 @@
+class Haveged < DebianSourceFormula
+  url 'http://backports.debian.org/debian-backports/pool/main/h/haveged/haveged_1.1-2~bpo60+1.dsc'
+  md5 'b043192e2accab197ec01c1f4773fa77'
+end
