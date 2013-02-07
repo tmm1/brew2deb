2c3b2a44d891b709e81ad93a24dab491bc166865
Aman Gupta <aman@tmm1.net>
2013-02-07 04:22:31 -0800

    pstree: bump version

--- packages/pstree/formula.rb
+++ packages/pstree/formula.rb
@@ -1,10 +1,10 @@
 class Pstree < DebianFormula
   homepage 'http://www.thp.uni-duisburg.de/pstree/'
-  url 'git://github.com/tmm1/pstree', :sha => '08531ba'
+  url 'git://github.com/tmm1/pstree', :sha => '44eb6c2'
 
   name 'gpstree'
-  version '2.33+github1'
-  description 'a better pstree than psmisc'
+  version '2.33+github2'
+  description 'better than psmisc'
 
   def build
     make 'pstree'
