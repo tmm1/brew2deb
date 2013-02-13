55afbb2dbdd6d942ba9055269c8349302b75ca6b
Ben Burkert <ben@benburkert.com>
2013-02-12 17:34:20 -0800

    Add a formula for tomcat7 on Ubuntu.

---
+++ packages/tomcat7/formula.rb
@@ -0,0 +1,7 @@
+# Sorry, tomcat7 is Ubuntu only :(
+
+class Tomcat7 < DebianSourceFormula
+  url 'http://archive.ubuntu.com/ubuntu/pool/main/t/tomcat7/tomcat7_7.0.34-0ubuntu1.dsc'
+  md5 'e0fd89009ac395f73af6761bf160422e'
+  version '7.0.34-0ubuntu1'
+end
