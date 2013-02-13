09b8763a81e420af287e0e91d7de87bf89b04927
Scott J. Goldman <scottjg@github.com>
2013-02-12 19:04:59 -0800

    libgithub: bump verison

--- packages/libgithub/formula.rb
+++ packages/libgithub/formula.rb
@@ -1,9 +1,9 @@
 class Libgithub < DebianFormula
   homepage 'https://github.com/github/libgithub'
-  url 'git@github.com:github/libgithub', :sha => '7735b20'
+  url 'git@github.com:github/libgithub', :sha => 'cb16f91'
 
   name 'libgithub'
-  version "1.0.1"
+  version "1.0.2"
   section 'libs'
   description 'Utility library for writing C at GitHub'
 
