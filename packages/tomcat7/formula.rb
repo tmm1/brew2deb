d15e0eeca6b5236787b28bddd33885b5f1cec58f
Ben Burkert <ben@benburkert.com>
2013-02-12 18:19:20 -0800

    Add the build dependencies to tomcat7.

--- packages/tomcat7/formula.rb
+++ packages/tomcat7/formula.rb
@@ -4,4 +4,14 @@ class Tomcat7 < DebianSourceFormula
   url 'http://archive.ubuntu.com/ubuntu/pool/main/t/tomcat7/tomcat7_7.0.34-0ubuntu1.dsc'
   md5 'e0fd89009ac395f73af6761bf160422e'
   version '7.0.34-0ubuntu1'
+
+  build_depends \
+    'default-jdk',
+    'ant-optional',
+    'maven-repo-helper (>> 1.0.1)',
+    'libecj-java',
+    'javahelper',
+    'junit4',
+    'libjstl1.1-java',
+    'libjakarta-taglibs-standard-java'
 end
