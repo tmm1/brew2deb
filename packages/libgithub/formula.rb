be895276f17e183019b0d37184f9ff44912eea24
Jesse Newland <jesse@github.com>
2013-02-15 10:12:25 -0500

    bump libgithub /cc github/libgithub#6

--- packages/libgithub/formula.rb
+++ packages/libgithub/formula.rb
@@ -1,6 +1,6 @@
 class Libgithub < DebianFormula
   homepage 'https://github.com/github/libgithub'
-  url 'git@github.com:github/libgithub', :sha => '6663d25'
+  url 'git@github.com:github/libgithub', :sha => 'bb37a5777750fbb126d8c6249ad4a26fba968056'
 
   name 'libgithub'
   version "1.0.3"
