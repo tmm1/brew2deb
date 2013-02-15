ab66980495d3c4f529c1f74d791e8a8769499502
Scott J. Goldman <scottjg@github.com>
2013-02-15 12:11:45 -0800

    bump toiletfs to v0.6

--- packages/toiletfs/formula.rb
+++ packages/toiletfs/formula.rb
@@ -1,11 +1,11 @@
 class Toiletfs < DebianFormula
   homepage 'http://github.com/scottjg/toiletfs'
-  url 'https://github.com/scottjg/toiletfs/tarball/v0.2'
+  url 'https://github.com/scottjg/toiletfs/tarball/v0.6'
   md5 '158a2a67c04ea7cb6e2f4bc14bca0adf'
 
   name 'toiletfs'
   section 'utils'
-  version '0.2'
+  version '0.6'
   description 'FUSE filesystem for storing coredumps'
 
   build_depends 'libfuse-dev'
