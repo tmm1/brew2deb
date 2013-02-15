6d0c16fef80eabcaa93b333e500f46d7b98509c9
Jesse Newland <jesse@github.com>
2013-02-15 15:28:20 -0500

    fix md5

--- packages/toiletfs/formula.rb
+++ packages/toiletfs/formula.rb
@@ -1,7 +1,7 @@
 class Toiletfs < DebianFormula
   homepage 'http://github.com/scottjg/toiletfs'
   url 'https://github.com/scottjg/toiletfs/tarball/v0.6'
-  md5 '158a2a67c04ea7cb6e2f4bc14bca0adf'
+  md5 '37bdaa99ffecb50683e28df2cb79d5bd'
 
   name 'toiletfs'
   section 'utils'
