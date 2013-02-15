2dbd591e6a5936310a872274e1eb9eb5e97aa467
Jesse Newland <jesse@github.com>
2013-02-15 11:38:21 -0500

    add md5 and deps

--- packages/augeas-tools/formula.rb
+++ packages/augeas-tools/formula.rb
@@ -1,7 +1,10 @@
 class AugeasTools < DebianSourceFormula
   url 'http://archive.ubuntu.com/ubuntu/pool/main/a/augeas/augeas_0.8.1-2.dsc'
+  md5 'b57393e8a6df54758c71bc89ca360c33'
 
   version '0.8.1-2'
+
+  build_depends %w(chrpath naturaldocs texlive-latex-base)
 
   def package
     FileUtils.mkdir_p(HOMEBREW_WORKDIR+'pkg')
