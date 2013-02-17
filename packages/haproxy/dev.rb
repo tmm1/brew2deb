9d00788d088488b11bbd5a1fb18b0b97de1dfb95
Alex Howells <alex@howells.me>
2013-02-17 02:32:43 +0000

    add new formula for HAProxy 1.5-dev with SSL support in as dev.rb

---
+++ packages/haproxy/dev.rb
@@ -0,0 +1,27 @@
+class HAProxy < DebianFormula
+  homepage 'http://haproxy.1wt.eu/'
+  url 'http://haproxy.1wt.eu/download/1.5/src/devel/haproxy-1.5-dev17.tar.gz'
+  md5 'b8deab9989e6b9925410b0bc44dd4353'
+
+  name 'haproxy'
+  version '1.5-dev17+github2'
+  section 'net'
+  description 'The Reliable, High Performance TCP/HTTP Load Balancer'
+
+  build_depends ['libpcre3-dev','libssl-dev','zlib1g-dev']
+  config_files '/etc/haproxy/haproxy.cfg'
+  requires_user 'haproxy', :remove => false
+
+  def build
+    make 'TARGET' => 'linux26', 'CPU' => 'native', 'PREFIX' => '/usr', 'USE_CTTPROXY' => '1', 'USE_PCRE' => '1', 'USE_OPENSSL' => '1', 'USE_ZLIB' => '1'
+  end
+
+  def install
+    make :install, 'DESTDIR' => destdir, 'PREFIX' => '/usr', 'DOCDIR' => '/usr/share/doc/haproxy'
+
+    (etc/'haproxy').install_p 'examples/haproxy.cfg'
+    (etc/'haproxy').install_p 'examples/errorfiles/', 'errors'
+    (etc/'default').install_p workdir/'default-haproxy', 'haproxy'
+    (etc/'init.d').install_p  workdir/'init.d-haproxy',  'haproxy'
+  end
+end
