ae0ebf65a641e5342990cee7176be39cb2a3f740
Yaroslav Shirokov <slava@github.com>
2013-02-20 13:23:15 -0800

    Bumping the deb version to match

--- packages/s3cmd/formula.rb
+++ packages/s3cmd/formula.rb
@@ -3,7 +3,7 @@ class S3cmd < DebianFormula
 
   name 's3cmd'
   section 'utils'
-  version '1.1.0-master+github6'
+  version '1.5.0-alpha1+github1'
   description 'command-line Amazon S3 client'
 
   build_depends 'python'
