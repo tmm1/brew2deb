e32834f7fb4c09d35b44109050a1c420f515ab8f
Corey Donohoe <atmos@atmos.org>
2013-02-19 22:43:00 -0800

    try compiling ffmpeg

--- packages/ffmpeg/formula.rb
+++ packages/ffmpeg/formula.rb
@@ -1,7 +1,8 @@
 class Ffmpeg < DebianFormula
-  homepage 'http://ffmpeg.org/'
+  name 'ffmpeg'
   url 'http://ffmpeg.org/releases/ffmpeg-1.1.2.tar.bz2'
   sha1 '1fb1c2019ff13440b3670dc83846c654b245c4c9'
+  homepage 'http://ffmpeg.org/'
 
   version "1.1.2-github1"
   description "ffmpeg turns one thing into another thing"
