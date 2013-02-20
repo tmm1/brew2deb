609aac244f97025421cd56e76f6241812f39b282
Corey Donohoe <atmos@atmos.org>
2013-02-19 21:59:40 -0800

    use a real lame release number

--- packages/lame/formula.rb
+++ packages/lame/formula.rb
@@ -2,7 +2,7 @@ class Lame < DebianFormula
   homepage 'http://lame.sourceforge.net/'
   url 'http://downloads.sourceforge.net/sourceforge/lame/lame-3.99.5.tar.gz'
   sha1 '03a0bfa85713adcc6b3383c12e2cc68a9cfbf4c4'
-  version "lame-github1"
+  version "3.99.5-github2"
   description "mp3 codecs for github.fm"
 
   def install
