083eddaf5ce58f51d6f56d323378927ea75f50e8
Corey Donohoe <atmos@atmos.org>
2013-02-19 23:33:11 -0800

    make them github.fm specific

--- packages/ffmpeg-github/formula.rb
+++ packages/ffmpeg-github/formula.rb
@@ -1,14 +1,14 @@
-class FfmpegGitHub < DebianFormula
-  name 'ffmpeg-github'
+class FfmpegGitHubFM < DebianFormula
+  name 'ffmpeg-github.fm'
   url 'http://ffmpeg.org/releases/ffmpeg-1.1.2.tar.bz2'
   sha1 '1fb1c2019ff13440b3670dc83846c654b245c4c9'
   homepage 'http://ffmpeg.org/'
 
-  version "1.1.2-github1"
+  version "1.1.2+github1"
   description "ffmpeg turns one thing into another thing"
 
-  depends "lame-github"
-  build_depends "lame-github"
+  depends "lame-github.fm"
+  build_depends "lame-github.fm"
 
   def build
     configure "--enable-libmp3lame",
