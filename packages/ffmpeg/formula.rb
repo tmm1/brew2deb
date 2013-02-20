47382dec2a5cd1eb0dfcfc7fdfd85b99206695ef
Corey Donohoe <atmos@atmos.org>
2013-02-19 22:32:18 -0800

    try building ffmpeg too

---
+++ packages/ffmpeg/formula.rb
@@ -0,0 +1,20 @@
+class Ffmpeg < DebianFormula
+  homepage 'http://ffmpeg.org/'
+  url 'http://ffmpeg.org/releases/ffmpeg-1.1.2.tar.bz2'
+  sha1 '1fb1c2019ff13440b3670dc83846c654b245c4c9'
+
+  version "1.1.2-github1"
+  description "ffmpeg turns one thing into another thing"
+
+  depends "lame"
+
+  def install
+    system "./configure", "--enable-libmp3lame",
+                          "--enable-nonfree",
+                          "--enable-pthreads",
+                          "--enable-openssl",
+                          "--prefix=#{prefix}"
+
+    system "make install"
+  end
+end
