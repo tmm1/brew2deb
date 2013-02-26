7f1329e14c7c2b70947ff8b0ce9b74ec91dfd56c
Yaroslav Shirokov <slava@github.com>
2013-02-26 15:42:25 -0800

    Set the origin of the package back to upstream

--- packages/s3cmd/formula.rb
+++ packages/s3cmd/formula.rb
@@ -1,9 +1,9 @@
 class S3cmd < DebianFormula
-  url 'https://github.com/sshirokov/s3cmd.git', :tag => 'v1.5.0-alpha1-github2'
+  url 'https://github.com/s3tools/s3cmd.git', :tag => 'a0f0f4fd84f528fd3fdb9df9b2c5414f01ad347e'
 
   name 's3cmd'
   section 'utils'
-  version '1.5.0-alpha1+github2'
+  version '1.5.0-alpha1+github3'
   description 'command-line Amazon S3 client'
 
   build_depends 'python'
