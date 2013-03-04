a4c6cbf2395e39da7b9dc152580bf7766087f5c6
David Calavera <david.calavera@gmail.com>
2013-03-04 12:59:44 -0800

    Put the rubyencoder formula inside its directory.

--- lib/debian_formula.rb
+++ lib/debian_formula.rb
@@ -456,28 +456,3 @@ class DebianSourceFormula < DebianFormula
     end
   end
 end
-
-class RubyEncoder < DebianFormula
-  def self.encoder_version(version)
-    plain_version = version.sub('.', '')
-
-    self.url "https://github-enterprise.s3.amazonaws.com/util/rubyencoder-#{version}-linux.tar.gz"
-    self.name "rubyencoder#{plain_version}"
-    self.version "#{version}+github1"
-  end
-
-  section 'libs'
-
-  depends \
-    'libgphoto2-2',
-    'libsane',
-    'ia32-libs-multiarch',
-    'ia32-libs'
-
-  def build
-  end
-
-  def install
-    bin.install_p 'bin/rubyencoder', self.class.name
-  end
-end
--- packages/rubyencoder/rubyencoder_1_4.rb
+++ packages/rubyencoder/rubyencoder_1_4.rb
@@ -1,3 +1,5 @@
+require 'rubyencoder'
+
 class RubyEncoder14 < RubyEncoder
   encoder_version '1.4'
   md5 '917c58a83fbc32bcfd912fec7a314b89'
