84b90a5784a7f8ec7870dc7306ed1dff97db5370
David Calavera <david.calavera@gmail.com>
2013-03-04 11:00:09 -0800

    Add formulas to build packages for Ruby Encoder.

---
+++ packages/rubyencoder/rubyencoder_1_3.rb
@@ -0,0 +1,4 @@
+class RubyEncoder13 < RubyEncoder
+  encoder_version '1.3'
+  md5 'c6cd924c7ad81660a2dfc5efc7d20d42'
+end
---
+++ packages/rubyencoder/rubyencoder_1_4.rb
@@ -0,0 +1,4 @@
+class RubyEncoder14 < RubyEncoder
+  encoder_version '1.4'
+  md5 '917c58a83fbc32bcfd912fec7a314b89'
+end
