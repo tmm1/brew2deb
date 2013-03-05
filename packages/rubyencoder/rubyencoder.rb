7dd33a465eebeb2fcacd19ebf9f1f74376227f6f
David Calavera <david.calavera@gmail.com>
2013-03-04 16:00:12 -0800

    Copy the RubyEncoder bin files.
    This excludes rgloader and extras that we don't need.

--- packages/rubyencoder/rubyencoder.rb
+++ packages/rubyencoder/rubyencoder.rb
@@ -1,8 +1,11 @@
 class RubyEncoder < DebianFormula
+  attr_rw :package_name
+
   def self.encoder_version(version)
     plain_version = version.sub('.', '')
 
-    self.url "https://github-enterprise.s3.amazonaws.com/util/rubyencoder-#{version}-linux.tar.gz"
+    self.package_name "rubyencoder-#{version}"
+    self.url "https://github-enterprise.s3.amazonaws.com/util/#{package_name}-linux.tar.gz"
     self.name "rubyencoder#{plain_version}"
     self.version "#{version}+github1"
   end
@@ -20,9 +23,16 @@ class RubyEncoder < DebianFormula
   end
 
   def install
-    name = self.class.name
+    package_name = self.class.package_name
 
-    prefix.install Dir['*']
-    ln_s((prefix / 'bin/rubyencoder'), (bin / name))
+    local = prefix / 'local'
+    package_path = local / package_name
+    package_path.mkpath
+
+    build_path = HOMEBREW_WORKDIR / 'tmp-build' / package_name
+
+    Dir[build_path + 'bin'].each do |file|
+      FileUtils.cp_r file, package_path
+    end
   end
 end
