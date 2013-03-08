5ae574ea208301ab7f326a6cf386d232be3a9a16
Jesse Newland <jesse@github.com>
2013-03-08 09:42:40 -0800

    bump version of megacli

--- packages/megacli/formula.rb
+++ packages/megacli/formula.rb
@@ -1,11 +1,9 @@
 class MegacliFormula < DebianFormula
   homepage 'http://www.lsi.com/support/products/Pages/MegaRAIDSAS8480E.aspx'
-  # download this manually (click through the EULA)
-  url 'http://www.lsi.com/downloads/Public/MegaRAID%20Common%20Files/8.02.16_MegaCLI.zip'
-  md5 '651f7250e0a64d94cafc08e4eb154740'
+  url 'https://s3.amazonaws.com/breakin-deps/8.07.06_MegaCLI.zip'
 
   name 'megacli'
-  version '8.02.16+github1'
+  version '8.07.06+github1'
   section 'alien'
   description 'MegaCli SAS RAID Management Utility.'
 
@@ -18,13 +16,11 @@ class MegacliFormula < DebianFormula
     'srvadmin-storelib-sysfs'
 
   def build
-    chdir 'LINUX' do
-      safe_system 'unzip', 'MegaCliLin.zip'
-    end
+    #lmao
   end
 
   def install
-    rpm_path = File.expand_path("LINUX/MegaCli-8.02.16-1.i386.rpm")
+    rpm_path = File.expand_path("Linux/MegaCli-8.07.06-1.noarch.rpm")
     chdir destdir do
       sh "rpm2cpio #{rpm_path} | cpio -d -i"
     end
