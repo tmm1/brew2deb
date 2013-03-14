e0e90c3c560b19fff90ada4d8c96cd8c72e3ec3f
Jeff King <peff@peff.net>
2013-03-14 04:13:06 -0400

    git: build off of new v1.8.1.5-series tag

--- packages/git/formula.rb
+++ packages/git/formula.rb
@@ -1,10 +1,10 @@
 class Git < DebianFormula
-  url 'git@github.com:github/git.git', :tag => 'github-1.7.10-29'
+  url 'git@github.com:github/git.git', :tag => 'github-1.8.1.5-1'
   homepage 'http://git-scm.com'
 
   section 'vcs'
   name 'git'
-  version '2:1.7.10-1+github29'
+  version '2:1.8.1.5-1+github1'
   description <<-DESC
     The Git DVCS with custom patches and bugfixes for GitHub.
   DESC
