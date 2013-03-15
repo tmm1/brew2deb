760df450593f760e816ed3e1357cb2150de522c0
Scott J. Goldman <scottjg@github.com>
2013-03-15 01:46:56 -0700

    libmicrohttpd: bump version to fix quiesce issues

--- packages/libmicrohttpd/formula.rb
+++ packages/libmicrohttpd/formula.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Libmicrohttpd < DebianFormula
   homepage 'http://www.gnu.org/software/libmicrohttpd/'
-  url 'http://github.com/scottjg/libmicrohttpd.git', :sha => '0afa79261f'
+  url 'http://github.com/scottjg/libmicrohttpd.git', :sha => '5847906'
 
   name 'libmicrohttpd'
   section 'libs'
-  version '0.9.25+github2'
+  version '0.9.25+github3'
   description 'HTTP server library'
 
   conflicts 'libmicrohttpd5'
