f1d85bae72dd6fc904ae7f7f6d3b277f2f5a473b
Scott J. Goldman <scottjg@github.com>
2013-03-15 02:12:18 -0700

    libmicrohttpd: bump version to fix spinning select loop on quiesce

--- packages/libmicrohttpd/formula.rb
+++ packages/libmicrohttpd/formula.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Libmicrohttpd < DebianFormula
   homepage 'http://www.gnu.org/software/libmicrohttpd/'
-  url 'http://github.com/scottjg/libmicrohttpd.git', :sha => '5847906'
+  url 'http://github.com/scottjg/libmicrohttpd.git', :sha => '865a559'
 
   name 'libmicrohttpd'
   section 'libs'
-  version '0.9.25+github3'
+  version '0.9.25+github4'
   description 'HTTP server library'
 
   conflicts 'libmicrohttpd5'
