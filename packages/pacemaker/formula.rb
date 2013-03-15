f86a071e27b20fc5424119f260d7cd7df58d76c8
Jesse Newland <jesse@github.com>
2013-03-15 11:31:36 -0700

    5058 and 5072

--- packages/pacemaker/formula.rb
+++ packages/pacemaker/formula.rb
@@ -1,7 +1,7 @@
 class Pacemaker < DebianSourceFormula
   url 'http://archive.ubuntu.com/ubuntu/pool/main/p/pacemaker/pacemaker_1.1.7-1ubuntu2.dsc'
   md5 'a37eb07651fa7c7fc20cdd97b98df63c'
-  version '1.1.7-2github2'
+  version '1.1.7-2github3'
   replaces 'crmsh, libqb, libqb-dev'
   conflicts 'crmsh, libqb, libqb-dev'
 
@@ -17,10 +17,14 @@ class Pacemaker < DebianSourceFormula
 
   def patches
     [
+      #http://bugs.clusterlabs.org/show_bug.cgi?id=5058
+      '54266cb5095c68a3afac0be9be93718435352d47.patch',
+      #http://bugs.clusterlabs.org/show_bug.cgi?id=5063
+      'b3c15a8dae467ed8f76797e5f6b59944b0567bb5.patch',
+      '3ab86ab9e8e63c920f7cf493f54eef7e8dfee32a.patch',
       #http://bugs.clusterlabs.org/show_bug.cgi?id=5072
       'fc03be02bf3a045babfe8233cbc99227da71d024.patch',
-      #http://bugs.clusterlabs.org/show_bug.cgi?id=5063
-      '54266cb5095c68a3afac0be9be93718435352d47.patch'
+      'd447452870f97a5032a6ec59f9722cc8e14ba402.patch',
     ]
   end
 end
