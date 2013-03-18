02b910dbe54d8f7c03cd2af20382647a3b5f66ed
Scott J. Goldman <scottjg@github.com>
2013-03-18 15:58:16 -0700

    haproxy1.5: build with openssl

--- packages/haproxy/1.5.rb
+++ packages/haproxy/1.5.rb
@@ -4,16 +4,20 @@ class Haproxy < DebianFormula
   md5 'b8deab9989e6b9925410b0bc44dd4353'
 
   name 'haproxy'
-  version '1.5.dev17+github1'
+  version '1.5.dev17+github2'
   section 'net'
   description 'The Reliable, High Performance TCP/HTTP Load Balancer'
 
   build_depends 'libpcre3-dev'
+  build_depends 'libssl-dev'
+
+  depends 'libssl0.9.8'
+
   config_files '/etc/haproxy/haproxy.cfg'
   requires_user 'haproxy', :remove => false
 
   def build
-    make 'TARGET' => 'linux26', 'CPU' => 'native', 'USE_PCRE' => '1', 'PREFIX' => '/usr'
+    make 'TARGET' => 'linux26', 'CPU' => 'native', 'USE_PCRE' => '1', 'USE_OPENSSL' => '1', 'PREFIX' => '/usr'
   end
 
   def install
