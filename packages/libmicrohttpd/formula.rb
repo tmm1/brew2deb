eb080a12458bc111051ffb2f5a718bcb61dbd9a1
Scott J. Goldman <scottjg@github.com>
2013-03-22 02:31:27 -0700

    libmicrohttpd: bump version to fix pipe leak in forking daemons
    /cc github/codeload#2

--- packages/libmicrohttpd/formula.rb
+++ packages/libmicrohttpd/formula.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Libmicrohttpd < DebianFormula
   homepage 'http://www.gnu.org/software/libmicrohttpd/'
-  url 'http://github.com/scottjg/libmicrohttpd.git', :sha => '865a559'
+  url 'http://github.com/scottjg/libmicrohttpd.git', :sha => '0da64d3'
 
   name 'libmicrohttpd'
   section 'libs'
-  version '0.9.25+github4'
+  version '0.9.25+github5'
   description 'HTTP server library'
 
   conflicts 'libmicrohttpd5'
