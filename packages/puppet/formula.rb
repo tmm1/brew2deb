461208193d79e4b5d3add9c0a44f0f586373dd8f
James Fryman <fryman@github.com>
2013-03-22 21:04:42 -0500

    tag with our version number

--- packages/puppet/formula.rb
+++ packages/puppet/formula.rb
@@ -2,7 +2,7 @@ class Puppet < DebianFormula
   url 'https://github.com/jfryman/massive-octo-bear.git'
 
   name 'puppet'
-  version '2.7.21'
+  version '2.7.21+github1'
   description "Empty dummy package for Puppet"
 
   def build
