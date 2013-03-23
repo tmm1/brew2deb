70cdc7ada5f9d974bc9a7d56fc6b735ec0ea9e58
James Fryman <fryman@github.com>
2013-03-22 20:03:58 -0500

    add build depends

--- packages/puppetdb/formula.rb
+++ packages/puppetdb/formula.rb
@@ -1,4 +1,11 @@
 class PuppetDB < DebianSourceFormula
   url 'https://apt.puppetlabs.com/pool/squeeze/main/p/puppetdb/puppetdb_1.1.1-1puppetlabs1.dsc'
   md5 'c9c1f847e464992ce7996be2c325cd74'
+
+  version '1.1.1+github3'
+
+  build_depends \
+    'leiningen',
+    'puppet',
+    'facter'
 end
