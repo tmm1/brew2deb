44829fb84cbbb26b81237f1feaaa7cd47f3b7bfa
Scott J. Goldman <scottjg@github.com>
2013-04-07 01:57:49 -0700

    openssh: trust, but verify

--- packages/openssh/formula.rb
+++ packages/openssh/formula.rb
@@ -1,7 +1,7 @@
 class OpenSSH < DebianSourceFormula
   url 'http://ftp.de.debian.org/debian/pool/main/o/openssh/openssh_5.5p1-6+squeeze1.dsc'
   md5 'b674c09fe06caa7b7735d05780813ae6'
-  version '1:5.5p1-6+squeeze1+github9'
+  version '1:5.5p1-6+squeeze1+github10'
 
   def patches
     [
