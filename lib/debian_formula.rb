177d6c7b75c3924396a3b732301371d604ea7fec
Aman Gupta <aman@tmm1.net>
2013-04-08 20:54:19 -0700

    use compat level 8

--- lib/debian_formula.rb
+++ lib/debian_formula.rb
@@ -292,14 +292,14 @@ CONTROL
         tmpdir = Dir.mktmpdir('brew2deb-shlibs')
         Dir.chdir tmpdir do
           FileUtils.mkdir_p 'debian'
+
+          File.open('debian/compat','w') { |f| f.puts '8' }
           File.open('debian/control','w'){ |f| f.puts "Source: #{name}\nPackage: #{name}" }
           out = `dpkg-gensymbols -P#{destdir} -p#{name} -v#{self.class.version} 2>&1 && fakeroot dh_shlibdeps -P#{destdir} -p#{name} 2>&1`
           if $?.exitstatus != 0
             opoo "Auto-calculation of shared library dependencies failed\n    #{out.split("\n").join("\n    ")}"
-          end
-
-          if File.exists?('debian/substvars')
-            File.read('debian/substvars').strip.gsub('shlibs:Depends=', '').split(', ').each do |dep|
+          elsif File.exists?("debian/#{name}.substvars") && (substvars = File.read("debian/#{name}.substvars"))
+            substvars.strip.gsub('shlibs:Depends=', '').split(', ').each do |dep|
               next if dep =~ /^#{name} /
               opts += ["--depends", dep]
             end
