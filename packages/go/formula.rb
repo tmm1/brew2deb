2829b6e750be2627711be2af59e671e2d6601ead
Scott J. Goldman <scottjg@github.com>
2013-04-10 00:44:49 -0700

    bump Go package to 1.0.3

--- packages/go/formula.rb
+++ packages/go/formula.rb
@@ -2,11 +2,11 @@ require 'formula'
 
 class Go < DebianFormula
   homepage 'http://www.golang.org'
-  url 'http://go.googlecode.com/files/go1.0.2.linux-amd64.tar.gz'
-  md5 'db580c82eeb15fd98b6eb4afd1055459'
+  url 'https://go.googlecode.com/files/go1.0.3.linux-amd64.tar.gz'
+  sha1 '97299d1989936b90d0c51ec8028d1f5dca8df6b6'
 
   name 'go'
-  version '1.0.2'
+  version '1.0.3'
   section 'main'
   description 'An open source programming environment from Google'
 
@@ -14,7 +14,10 @@ class Go < DebianFormula
   end
 
   def install
-    system "mkdir -p #{destdir}/local"
-    system "cd .. && mv go #{destdir}/local/"
+    system "mkdir -p #{destdir}/usr/local #{destdir}/usr/bin"
+    system "cd .. && mv go #{destdir}/usr/local/"
+    system "ln -s /usr/local/go/bin/go    #{destdir}/usr/bin/"
+    system "ln -s /usr/local/go/bin/gofmt #{destdir}/usr/bin/"
+    system "ln -s /usr/local/go/bin/godoc #{destdir}/usr/bin/"
   end
 end
